d94e488ad7ab6e96c02c5b9a6f555a883fbb6615
RiccardoBravin <riccardo.bravin.1@studenti.unipd.it>
2021-05-04 08:28:50 +0200

    Initial commit

---
+++ myAdapter/HSet.java
@@ -0,0 +1,33 @@
+package myAdapter;
+public interface HSet extends HCollection{
+    
+    boolean add(Object o) throws UnsupportedOperationException, ClassCastException, NullPointerException, IllegalArgumentException;
+
+    boolean addAll(HCollection c) throws UnsupportedOperationException, ClassCastException, NullPointerException, IllegalArgumentException;
+
+    void clear() throws UnsupportedOperationException;
+
+    boolean contains(Object o) throws ClassCastException, NullPointerException;
+    
+    boolean containsAll(HCollection c) throws ClassCastException, NullPointerException;
+    
+    boolean equals(Object o);
+    
+    int hashCode();
+    
+    boolean isEmpty();
+
+    HIterator iterator();
+
+    boolean remove(Object o) throws ClassCastException, NullPointerException, UnsupportedOperationException;
+
+    boolean removeAll(HCollection c) throws UnsupportedOperationException, ClassCastException, NullPointerException;
+    
+    boolean retainAll(HCollection c) throws UnsupportedOperationException, ClassCastException, NullPointerException;
+
+    int size();
+
+    Object[] toArray();
+
+    Object[] toArray(Object [] a) throws ArrayStoreException, NullPointerException;
+}

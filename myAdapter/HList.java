7f2551eb00687bf0c1cf15b3ac7d52ecaaf71a8a
EliaFeltrin <elia.feltrin@studenti.unipd.it>
2021-05-04 17:27:44 +0200

    non funzia javaDoc

--- myAdapter/HList.java
+++ myAdapter/HList.java
@@ -1,11 +1,21 @@
 package myAdapter;
 
-import myAdapter.HCollection;
-
 public interface HList extends HCollection {
 
+    /**
+     * sos
+     * 
+     * @param index
+     * @param Element
+     * @throws UnsupportedOperationException
+     * @throws ClassCastException
+     * @throws NullPointerException
+     * @throws IllegalArgumentException
+     * @throws IndexOutOfBoundsException
+     */
     void add(int index, Object Element) throws UnsupportedOperationException, ClassCastException, NullPointerException, IllegalArgumentException, IndexOutOfBoundsException;
 
+    
     boolean add(Object obj) throws UnsupportedOperationException, ClassCastException, NullPointerException, IllegalArgumentException;
 
     boolean addAll(HCollection c) throws UnsupportedOperationException, ClassCastException, NullPointerException, IllegalArgumentException;
@@ -14,6 +24,7 @@ public interface HList extends HCollection {
 
     void clear();
 
+    
     boolean contains(Object o) throws ClassCastException, NullPointerException;
 
     boolean containsAll(HCollection c) throws ClassCastException, NullPointerException;
@@ -26,6 +37,12 @@ public interface HList extends HCollection {
 
     int indexOf(Object o) throws ClassCastException, NullPointerException;
 
+    /**
+     * Returns true if this list contains no elements.
+     * 
+     * @return true if this list contains no elements.
+
+     */
     boolean isEmpty();
 
     HIterator iterator();
@@ -45,7 +62,12 @@ public interface HList extends HCollection {
     boolean retainAll(HCollection c) throws UnsupportedOperationException, ClassCastException, NullPointerException;
 
     Object set(int index, Object element) throws UnsupportedOperationException, ClassCastException, NullPointerException, IllegalArgumentException, IndexOutOfBoundsException;
-
+    
+    /**
+     * Returns the number of elements in this list. If this list contains more than Integer.MAX_VALUE elements, returns Integer.MAX_VALUE.
+     * 
+     * @return the number of elements in this list.
+     */
     int size();
 
     HList subList(int fromIndex, int toIndex) throws IndexOutOfBoundsException;

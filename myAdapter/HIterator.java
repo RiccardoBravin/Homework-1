6dd4c81cfc0dc3ad21ac9603583babde6c5a9798
EliaFeltrin <elia.feltrin@studenti.unipd.it>
2021-05-04 10:05:55 +0200

    Hiterator

--- myAdapter/HIterator.java
+++ myAdapter/HIterator.java
@@ -1,4 +1,12 @@
 package myAdapter;
+
+import java.util.NoSuchElementException;
+
 public interface HIterator {
     
+    boolean hasNext();
+
+    Object next() throws NoSuchElementException;
+
+    void remove() throws UnsupportedOperationException, IllegalStateException;
 }

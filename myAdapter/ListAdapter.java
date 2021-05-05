fe885d93e401be0146cf9fb45fefaa94de45ba26
EliaFeltrin <elia.feltrin@studenti.unipd.it>
2021-05-05 11:39:26 +0200

    listadapter mothods

--- myAdapter/ListAdapter.java
+++ myAdapter/ListAdapter.java
@@ -1,5 +1,160 @@
 package myAdapter;
 
-public class ListAdapter implements HList, HCollection {
-    
+import java.util.Vector;
+
+public class ListAdapter implements HList {
+
+    //@Override
+    public void add(int index, Object Element) throws UnsupportedOperationException, ClassCastException,
+            NullPointerException, IllegalArgumentException, IndexOutOfBoundsException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    //@Override
+    public boolean add(Object obj)
+            throws UnsupportedOperationException, ClassCastException, NullPointerException, IllegalArgumentException {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    //@Override
+    public boolean addAll(HCollection c)
+            throws UnsupportedOperationException, ClassCastException, NullPointerException, IllegalArgumentException {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    //@Override
+    public boolean addAll(int index, HCollection c) throws UnsupportedOperationException, ClassCastException,
+            NullPointerException, IllegalArgumentException, IndexOutOfBoundsException {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    //@Override
+    public void clear() throws UnsupportedOperationException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    //@Override
+    public boolean contains(Object o) throws ClassCastException, NullPointerException {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    //@Override
+    public boolean containsAll(HCollection c) throws ClassCastException, NullPointerException {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    //@Override
+    public Object get(int index) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    //@Override
+    public int hasCode() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    //@Override
+    public int indexOf(Object o) throws ClassCastException, NullPointerException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    //@Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    //@Override
+    public HIterator iterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    //@Override
+    public int lastIndexOf(Object o) throws ClassCastException, NullPointerException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    //@Override
+    public HListIterator listIterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    //@Override
+    public HListIterator listIterator(int index) throws IndexOutOfBoundsException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    //@Override
+    public Object remove(int index) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    //@Override
+    public boolean remove(Object o) throws ClassCastException, NullPointerException, UnsupportedOperationException {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    //@Override
+    public boolean removeAll(HCollection c)
+            throws UnsupportedOperationException, ClassCastException, NullPointerException {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    //@Override
+    public boolean retainAll(HCollection c)
+            throws UnsupportedOperationException, ClassCastException, NullPointerException {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    //@Override
+    public Object set(int index, Object element) throws UnsupportedOperationException, ClassCastException,
+            NullPointerException, IllegalArgumentException, IndexOutOfBoundsException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    //@Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    //@Override
+    public HList subList(int fromIndex, int toIndex) throws IndexOutOfBoundsException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    //@Override
+    public Object[] toArray() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    //@Override
+    public Object[] toArray(Object[] a) throws ArrayStoreException, NullPointerException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+    private Vector vec;
 }

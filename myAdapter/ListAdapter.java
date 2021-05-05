9409769933eaf0e6b32dc96708702d8990c80412
EliaFeltrin <elia.feltrin@studenti.unipd.it>
2021-05-05 20:45:23 +0200

    update listadapter

--- myAdapter/ListAdapter.java
+++ myAdapter/ListAdapter.java
@@ -4,157 +4,229 @@ import java.util.Vector;
 
 public class ListAdapter implements HList {
 
-    //@Override
-    public void add(int index, Object Element) throws UnsupportedOperationException, ClassCastException,
-            NullPointerException, IllegalArgumentException, IndexOutOfBoundsException {
-        // TODO Auto-generated method stub
-        
+    public ListAdapter(){
+        vec = new Vector();
+    }
+    
+   
+    public void add(int index, Object Element) throws IndexOutOfBoundsException {
+        try {
+            vec.insertElementAt(Element, index);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new IndexOutOfBoundsException();
+        }
     }
 
-    //@Override
-    public boolean add(Object obj)
-            throws UnsupportedOperationException, ClassCastException, NullPointerException, IllegalArgumentException {
-        // TODO Auto-generated method stub
-        return false;
+   
+    public boolean add(Object obj) {
+        vec.addElement(obj);
+        return true;
     }
 
-    //@Override
-    public boolean addAll(HCollection c)
-            throws UnsupportedOperationException, ClassCastException, NullPointerException, IllegalArgumentException {
-        // TODO Auto-generated method stub
-        return false;
+   
+    public boolean addAll(HCollection c) throws NullPointerException {
+        if(c == null) throw new NullPointerException();
+
+        int oldSize = size();
+        Hiterator iter = c.iterator(); 
+
+        while(iter.hasNext()){
+            add(iter.next());
+        }
+
+        if(oldSize == vec.size()) return false; else return true;
     }
 
-    //@Override
-    public boolean addAll(int index, HCollection c) throws UnsupportedOperationException, ClassCastException,
-            NullPointerException, IllegalArgumentException, IndexOutOfBoundsException {
-        // TODO Auto-generated method stub
-        return false;
+   
+    public boolean addAll(int index, HCollection c) throws IndexOutOfBoundsException, NullPointerException {
+        if(c == null) throw new NullPointerException();
+
+        int oldSize = size();
+        HIterator iter = c.iterator();
+        int i = 0;
+
+        while(iter.hasNext){
+            add(index + i, c.next());
+            i++;
+        }
+
+        if(oldSize == vec.size()) return false; else return true;
     }
 
-    //@Override
-    public void clear() throws UnsupportedOperationException {
-        // TODO Auto-generated method stub
-        
+   
+    public void clear() {
+        vec.removeAllElements();
     }
 
-    //@Override
-    public boolean contains(Object o) throws ClassCastException, NullPointerException {
-        // TODO Auto-generated method stub
-        return false;
+   
+    public boolean contains(Object o) {
+        return vec.contains(o);
     }
 
-    //@Override
-    public boolean containsAll(HCollection c) throws ClassCastException, NullPointerException {
-        // TODO Auto-generated method stub
-        return false;
+   
+    public boolean containsAll(HCollection c) throws NullPointerException {
+        if(c == null) throw new  NullPointerException();
+
+        HIterator iter = c.iterator();
+
+        while(iter.hasNext()){
+            if(!contains(iter.next())) 
+                return false;
+        }
+
+        return true;
     }
 
-    //@Override
+
+    public boolean equals(Object o) {
+        if(o == null) return false;
+        if(!o.instanceOf(HList)) return false;
+        if(o.size() != size()) return false;
+
+        HIterator OIter = o.iterator();
+        HIterator iter = iterator();
+        while(iter.hasNext()){
+            if(!iter.next.equals(OIter.next())) return false;
+        }
+
+        return true;
+    }
+
+   
     public Object get(int index) {
-        // TODO Auto-generated method stub
-        return null;
+        try{
+            return vec.elementAt(index);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new IndexOutOfBoundsException();
+        }
     }
 
-    //@Override
+   
     public int hasCode() {
-        // TODO Auto-generated method stub
-        return 0;
+        int hasCode = 1;
+        HIterator iter = iterator();
+
+        while(iter.hasNext()){
+            Object obj = iter.next();
+            int increment = 0;
+            if(obj != null)
+                increment = obj.hashCode();
+            hasCode = 31*hasCode + increment;
+        }
+
+        return hasCode;
     }
 
-    //@Override
-    public int indexOf(Object o) throws ClassCastException, NullPointerException {
-        // TODO Auto-generated method stub
-        return 0;
+   
+    public int indexOf(Object o) {
+        return vec.indexOf(o);
     }
 
-    //@Override
+   
     public boolean isEmpty() {
-        // TODO Auto-generated method stub
-        return false;
+        return vec.isEmpty();
     }
 
-    //@Override
+   
     public HIterator iterator() {
         // TODO Auto-generated method stub
         return null;
     }
 
-    //@Override
-    public int lastIndexOf(Object o) throws ClassCastException, NullPointerException {
-        // TODO Auto-generated method stub
-        return 0;
+   
+    public int lastIndexOf(Object o) {
+        return vec.lastIndexOf(o);
     }
 
-    //@Override
+   
     public HListIterator listIterator() {
         // TODO Auto-generated method stub
         return null;
     }
 
-    //@Override
+   
     public HListIterator listIterator(int index) throws IndexOutOfBoundsException {
         // TODO Auto-generated method stub
         return null;
     }
 
-    //@Override
+   
     public Object remove(int index) {
-        // TODO Auto-generated method stub
-        return null;
+            try{
+                Object removed = vec.elementAt(index);
+                vec.removeElementAt(index);
+                return removed;
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new IndexOutOfBoundsException();
+            }
     }
 
-    //@Override
-    public boolean remove(Object o) throws ClassCastException, NullPointerException, UnsupportedOperationException {
-        // TODO Auto-generated method stub
-        return false;
+   
+    public boolean remove(Object o) {
+        return vec.removeElement(o);
     }
 
-    //@Override
-    public boolean removeAll(HCollection c)
-            throws UnsupportedOperationException, ClassCastException, NullPointerException {
-        // TODO Auto-generated method stub
-        return false;
+   
+    public boolean removeAll(HCollection c) throws NullPointerException {
+        if(c==null) throw new NullPointerException();
+
+        int oldSize = size();
+        HIterator iter = c.iterator();
+        while(iter.hasNext()){
+            vec.remove(iter.next());
+        }
+
+        if(oldSize == vec.size()) return false; else return true;
     }
 
-    //@Override
-    public boolean retainAll(HCollection c)
-            throws UnsupportedOperationException, ClassCastException, NullPointerException {
-        // TODO Auto-generated method stub
-        return false;
+   
+    public boolean retainAll(HCollection c) throws NullPointerException {
+        if(c == null) throw new NullPointerException();
+        
+        int oldSize = vec.size();
+        for(int i=0; i<vec.size(); i++){
+            if(!c.contains(vec.elementAt(i))){
+                vec.removeElementAt(i);
+                i--;
+            }
+        }
+        if(oldSize == vec.size()) return false; else return true;
     }
 
-    //@Override
-    public Object set(int index, Object element) throws UnsupportedOperationException, ClassCastException,
-            NullPointerException, IllegalArgumentException, IndexOutOfBoundsException {
-        // TODO Auto-generated method stub
-        return null;
+   
+    public Object set(int index, Object element) throws IndexOutOfBoundsException {
+        try{
+            Object removed = vec.elementAt(index);
+            vec.setElementAt(element, index);
+            return removed;
+        } catch (ArrayIndexOutOfBoundsException e ) {
+            throw new IndexOutOfBoundsException();
+        }
+        
     }
 
-    //@Override
+   
     public int size() {
-        // TODO Auto-generated method stub
-        return 0;
+        return vec.size();
     }
 
-    //@Override
+   
     public HList subList(int fromIndex, int toIndex) throws IndexOutOfBoundsException {
         // TODO Auto-generated method stub
         return null;
     }
 
-    //@Override
+   
     public Object[] toArray() {
         // TODO Auto-generated method stub
         return null;
     }
 
-    //@Override
+   
     public Object[] toArray(Object[] a) throws ArrayStoreException, NullPointerException {
         // TODO Auto-generated method stub
         return null;
     }
-
 
     private Vector vec;
 }
